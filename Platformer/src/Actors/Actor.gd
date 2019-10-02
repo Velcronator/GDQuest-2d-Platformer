e0060499a8b380d8e43b1ef2158e00f182f74211
velcronator <valacos7@gmail.com>
2019-10-02 23:57:18 +1000

    Cleaning up the code, Added jump and Movement

--- Platformer/src/Actors/Actor.gd
+++ Platformer/src/Actors/Actor.gd
@@ -1,13 +1,14 @@
-extends KinematicBody2D 
+extends KinematicBody2D
 class_name Actor
 
-var velocity: Vector2 = Vector2.ZERO
 
+const FLOOR_NORMAL: = Vector2.UP
 
-export var speed: = Vector2(300.0, 1000.0)
-export var gravity: float = 3000.0
+export var speed: = Vector2(400.0, 500.0)
+export var gravity: = 3500.0
+
+var _velocity: = Vector2.ZERO
+
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	move_and_slide(velocity)
-	
+	_velocity.y += gravity * delta
